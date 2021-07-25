7f376a16c7a98fe0d82e3cee3eaf34f7aa6cf8cd
Kai Koh <kaikoh95@gmail.com>
2021-07-25 12:20:00 +1200

    Develop: Removed old files.

--- README.md
+++
@@ -1,3 +0,0 @@
-# NOTE
-
-I will be revamping the entire site using either React.js or Angular.js
--- js/logic.js
+++
@@ -1,8 +0,0 @@
-function resizeAbout() {
-  document.getElementById("dark").style.width = "300px";
-  document.getElementById("dark").style.height = "175px";
-}
-
-function learnHowIOSMedium() {
-  window.location.href = 'https://medium.com/better-programming/how-to-build-ios-uis-programmatically-e0c4be2c3f24';
-}
